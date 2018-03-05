5e870577194f8c989d2bc8feec60debec4278bde
ValysyaPoncho <valentin.ivancikov@gmail.com>
2018-03-06 01:19:30 +0300

    11th solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,72 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.stack = [];
+    this.stack.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.stack.push("+");
+    this.stack.push(number);
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.stack.push("-");
+    this.stack.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stack.push("*");
+    this.stack.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stack.push("/");
+    this.stack.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.stack.push("^");
+    this.stack.push(number);
+    return this;
+  }
+
+  valueOf(stack) {
+    for(var i = this.stack.length; i > 0; i--)
+      if (this.stack[i] === "^") {
+        this.stack.splice(i - 1, 3, Math.pow(this.stack[i - 1], this.stack[i + 1]));
+        i++;
+      }
+
+    
+    for(var i = 0; i < this.stack.length; i++)
+      if (this.stack[i] === "/") {
+        this.stack.splice(i - 1, 3, this.stack[i - 1] / this.stack[i + 1]);
+        i--;
+      }
+
+    for(var i = 0; i < this.stack.length; i++)
+      if (this.stack[i] === "*") {
+        this.stack.splice(i - 1, 3, this.stack[i - 1] * this.stack[i + 1]);
+        i--;
+      }
+
+    for(var i = 0; i < this.stack.length; i++)
+      if (this.stack[i] === "-") {
+        this.stack.splice(i - 1, 3, this.stack[i - 1] - this.stack[i + 1]);
+        i--;
+      }
+
+    for(var i = 0; i < this.stack.length; i++)
+      if (this.stack[i] === "+") {
+        this.stack.splice(i - 1, 3, this.stack[i - 1] + this.stack[i + 1])
+        i--;
+      }
+
+    return this.stack.join("");
   }
 }
 
